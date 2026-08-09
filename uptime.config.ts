6d03d3ef75b73047d031fd44fc79832779de0aae
PastRain <wangshiyu3007@163.com>
2026-08-09 23:34:42 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -120,23 +120,23 @@ const workerConfig: WorkerConfig = {
 
 // const maintenances: MaintenanceConfig[] = []
 
-const maintenances: MaintenanceConfig[] = [
-  {
-    // [Optional] Monitor IDs to be affected by this maintenance
-    monitors: ['foo_monitor', 'bar_monitor'],
-    // [Optional] default to "Scheduled Maintenance" if not specified
-    title: 'Test Maintenance',
-    // Description of the maintenance, will be shown at status page
-    body: 'This is a test maintenance, server software upgrade',
-    // Start time of the maintenance, in UNIX timestamp or ISO 8601 format
-    start: '2020-01-01T00:00:00+08:00',
-    // [Optional] end time of the maintenance, in UNIX timestamp or ISO 8601 format
-    // if not specified, the maintenance will be considered as on-going
-    end: '2050-01-01T00:00:00+08:00',
-    // [Optional] color of the maintenance alert at status page, default to "yellow"
-    color: 'blue',
-  },
-]
+// const maintenances: MaintenanceConfig[] = [
+//   {
+//     // [Optional] Monitor IDs to be affected by this maintenance
+//     monitors: ['foo_monitor', 'bar_monitor'],
+//     // [Optional] default to "Scheduled Maintenance" if not specified
+//     title: 'Test Maintenance',
+//     // Description of the maintenance, will be shown at status page
+//     body: 'This is a test maintenance, server software upgrade',
+//     // Start time of the maintenance, in UNIX timestamp or ISO 8601 format
+//     start: '2020-01-01T00:00:00+08:00',
+//     // [Optional] end time of the maintenance, in UNIX timestamp or ISO 8601 format
+//     // if not specified, the maintenance will be considered as on-going
+//     end: '2050-01-01T00:00:00+08:00',
+//     // [Optional] color of the maintenance alert at status page, default to "yellow"
+//     color: 'blue',
+//   },
+// ]
 
 // Don't edit this line
 export { maintenances, pageConfig, workerConfig }
